df6e2570b31e1ce7f2dbacc7d0243f7a06fae135
Andrey Balabokhin <abalabokhin@gmail.com>
2018-03-14 19:40:04 -0400

    first version of the script

--- hard_reset.py
+++ hard_reset.py
@@ -1,2 +1,55 @@
-#!/usr/bin/python
-print("HW!");
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+def setup_pins(pins):
+    GPIO.setmode(GPIO.BCM)
+    for i in pins:
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, GPIO.HIGH)
+
+def check_host(host):
+    line = "ping -c1 -w2 " + str(host)
+    print(line)
+    status,result = subprocess.getstatusoutput(line)
+    if status == 0:
+        return True
+    else:
+        return False
+
+def down_host(pin):
+    GPIO.output(i, GPIO.LOW)
+    itime.sleep(6)
+    GPIO.output(i, GPIO.HIGH)
+    print(pin, "down")
+
+def up_host(pin):
+    GPIO.output(i, GPIO.LOW)
+    time.sleep(0.2)
+    GPIO.output(i, GPIO.HIGH)
+    print(pin, "up")
+
+def main():
+    pins = [16, 24]
+    hosts = ["192.168.0.150", "192.168.0.109"]
+    sleep_time = 6
+
+    setup_pins(pins)
+
+    while True:
+        for i in range(len(pins)):
+            if check_host(hosts[i]):
+                print(hosts[i], "ok")
+            else:
+                print(hosts[i], "is bad, reseting")
+                down_host(pins[i])
+                time.sleep(0.5)
+                up_host(pins[i])
+        
+        time.sleep(sleep_time)
+
+main()
+
+
